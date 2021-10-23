4c6ff38ad13cf1ddd0648e6edc2cde4fa9e0b498
Jorge <jzrueda@gmail.com>
2021-10-22 21:32:54 -0500

    Fix code error

--- src/middlewares/access.middleware.js
+++ src/middlewares/access.middleware.js
@@ -4,7 +4,7 @@ import ROUTES_ROLES from '../constants/routes-roles.contants.js'
 const validateAccess = (req, resp, next) => {
   let url = req.url.split('/api')[1].split('/').slice(0,3).join('/');
   const userData = resp.locals.userData;
-  const role = userData?.role ? '';
+  const role = userData?.role ?? '';
   const routesRoles = ROUTES_ROLES[url];
   const hasAccess = routesRoles && routesRoles.includes(role);
   if (hasAccess) {
